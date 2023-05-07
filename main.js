6bf6d86e40da8a6ddc4de20f2dd99043ccbc9d83
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-05-08 02:16:58 +0500

    section about JS is started

---
+++ main.js
@@ -0,0 +1,17 @@
+const section=document.querySelector(".make")
+const ul=document.querySelector(".navbar-list2")
+const li=document.querySelector(".navbar-item2")
+const link=document.querySelectorAll("a")
+// link.addEventListener("mouseover",event=>{
+//     link.style.backgroundColor="red"
+// })
+
+// link.addEventListener("",event=>{
+//     link.style.backgroundColor="red"
+// })
+
+link.forEach(event=>{
+ event.addEventListener("mouseover",event=>{
+    
+ })
+})
